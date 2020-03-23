01ab3c9635f3adfb1641d64828707a9ec763a36e
saiforahi <saiforahi@gmail.com>
2020-03-24 01:20:02 +0600

    Compression and file generation is completed excluding the huffman coding tree.

--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -1,9 +1,13 @@
 package controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Vector;
@@ -11,28 +15,33 @@ import classes.HuffmanNode;
 import classes.Symbol;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.StageStyle;
 
 public class MainViewController {
-	public static String path;
+	private Vector<Symbol> characters;
 	private File selectedFile;
 	@FXML
     private Button file_choose_button;
 	@FXML
-    private Label input_label;
-	@FXML
 	private Button compress_btn;
+	@FXML
+	private TextField url_field;
+	@FXML
+	private Label size_label;
     @FXML
     void openChooser(ActionEvent event) {
     	FileChooser fil_chooser = new FileChooser(); 
     	fil_chooser.setTitle("Select a text file"); 
     	fil_chooser.getExtensionFilters().addAll(new ExtensionFilter("text Files", "*.txt"));
     	selectedFile = fil_chooser.showOpenDialog(file_choose_button.getScene().getWindow());
-    	String details="Selected File size "+selectedFile.length()+" bytes\n"+"File path: "+selectedFile.getAbsolutePath();
-		input_label.setText(details);
+    	url_field.setText(selectedFile.getAbsolutePath());
+    	size_label.setText("Size: "+selectedFile.length()+" bytes");
     }
     @FXML
     void compress(ActionEvent event) {
@@ -41,7 +50,7 @@ public class MainViewController {
 	    		
 	    		String data = new String(Files.readAllBytes(Paths.get(selectedFile.getAbsolutePath())));
 	    		System.out.println("File Size "+selectedFile.length()+" bytes");
-				Vector<Symbol> characters=new Vector<Symbol>();
+				characters=new Vector<Symbol>();
 				for(int index=0;index<data.length();index++) {
 					boolean found=false;
 					for(int index2=0;index2<characters.size();index2++) {
@@ -64,7 +73,6 @@ public class MainViewController {
 		            hn.right = null;  
 		            queue.add(hn); 
 		        } 
-		        HuffmanNode root = null; 
 		        // Here we will extract the two minimum value 
 		        // from the heap each time until 
 		        // its size reduces to 1, extract until 
@@ -86,20 +94,11 @@ public class MainViewController {
 		            ztemp3.left = xtemp1; 
 		            // second extracted node as the right child. 
 		            ztemp3.right = ytemp2; 
-		            // marking the f node as the root node. 
-		            root = ztemp3; 
 		            // add this node to the priority-queue. 
 		            queue.add(ztemp3);
 		        }
-				for(int index=0;index<characters.size();index++) {
-					get_path(queue.element(),characters.get(index).get_character(),"");
-					characters.get(index).set_path(MainViewController.path);
-				}
-				for(int index=0;index<characters.size();index++) {
-					System.out.println(characters.get(index).get_character()+"->"+characters.get(index).get_path());
-				}
-				//byte[] byteArray=
-				printCode(root,"");
+		        set_paths(queue.element(),"");
+		        generate_file(data);
 	    	}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -108,31 +107,67 @@ public class MainViewController {
     }
     @FXML
     public void initialize() {
-    	input_label.setText("No file selected");
+    	size_label.setText("Size: ");
+    	url_field.textProperty().addListener((obs, oldText, newText) -> {
+    		if(!newText.equals(null) && new File(url_field.getText()).exists()) {
+    			this.selectedFile=new File(url_field.getText());
+        		size_label.setText("Size: "+this.selectedFile.length()+" bytes");
+        	}
+        	else {
+        		size_label.setText("Not a file");
+        	}
+    	});
     }
-    public static void get_path(HuffmanNode node,char value,String s) {
-    	if(node.left==null && node.right==null && node.askii==(int)value) {
-    		MainViewController.path=s;
+    public void set_paths(HuffmanNode node,String s) {
+    	if(node.left == null && node.right == null) {
+    		for(int index=0;index<this.characters.size();index++) {
+    			if(this.characters.get(index).get_ascii_value()==node.askii) {
+    				this.characters.get(index).set_path(s);
+    				break;
+    			}
+    		}
     		return;
     	}
-    	get_path(node.left,value,s+"0");
-    	get_path(node.right,value,s+"1");
+    	set_paths(node.left,s+"0");
+    	set_paths(node.right,s+"1");
     }
-    //below snippet of code is took from https://www.geeksforgeeks.org/huffman-coding-greedy-algo-3/
-    public static void printCode(HuffmanNode root, String s) 
-    {
-        if (root.left == null && root.right == null) { 
-            // c is the character in the node 
-            //System.out.println((char)root.askii + ":" + s);
-            MainViewController.path=s;
-            return; 
-        }
-        // if we go to left then add "0" to the code.                       
-        // if we go to the right add"1" to the code. 
-        // recursive calls for left and 
-        // right sub-tree of the generated tree. 
-        printCode(root.left, s + "0"); 
-        printCode(root.right, s + "1"); 
-    } 
+    public void generate_file(String data) {
+    	String codes="";
+    	for(int index=0;index<data.length();index++) {
+    		for(int index2=0;index2<characters.size();index2++) {
+    			if(characters.get(index2).get_character()==data.charAt(index)) {
+    				codes+=characters.get(index2).get_path();
+    			}
+    		}
+    	}
+    	BitSet bitSet = new BitSet(codes.length());
+    	int bitcounter = 0;
+    	for(Character c : codes.toCharArray()) {
+    	    if(c.equals('1')) {
+    	        bitSet.set(bitcounter);
+    	    }
+    	    bitcounter++;
+    	}
+    	try { 
+            OutputStream os = new FileOutputStream(selectedFile.getParent()+"\\compressed.sas3"); 
+            os.write(bitSet.toByteArray()); 
+            System.out.println("Successfully"+ " byte inserted\n"+Arrays.toString(bitSet.toByteArray())); 
+            os.close();
+            showInformation("Alert","Completed");
+        } 
+        catch (Exception e) { 
+        	showInformation("Error", e.getMessage());
+        } 
+    }
+    
+    public static void showInformation(String title, String message) {
+    	//Runtime.getRuntime().exec("explorer.exe /select," + path);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.initStyle(StageStyle.UTILITY);
+        alert.setTitle("Information");
+        alert.setHeaderText(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 
 }
